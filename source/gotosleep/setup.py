ff901b335a6b7fb6c6828e0a0819636c415e8af1
Lewis Trowbridge <lewbob9927@gmail.com>
2021-08-07 21:56:37 +0100

    Altered default value handling when checking env variables

--- source/gotosleep/setup.py
+++ source/gotosleep/setup.py
@@ -41,16 +41,16 @@ def init_argparse():
     parser.add_argument("-m", "--maps-token", help="the API token supplied from Google Cloud Services. Overrides GTS_MAPS_TOKEN environment variable")
     parser.add_argument("-d", "--db-path", help="the path to the SQL database to use. Overrides GTS_DB_PATH environment variable", type=db_file)
     parser.add_argument("-l", "--log-dir", help="the directory to store logs in. Defaults to './logs'", type=log_dir, default="logs")
-    parser.add_argument("-s", "--support-server", help="the invite link of the support server used with this bot", default="")
+    parser.add_argument("-s", "--support-server", help="the invite link of the support server used with this bot")
     parser.add_argument("--sqlite", dest="sqlite", help="Signals to use SQLite database.", action="store_true")
     parser.add_argument("--mysql", dest="mysql", help="Signals to use MySQL database.", action="store_true")
     
     return parser
 
-def check_env_variable(variable_name: str):
-    env_value = os.getenv(variable_name)
+def check_env_variable(variable_name: str, default_value = None):
+    env_value = os.getenv(variable_name, default_value)
     if env_value == None:
-        sys.exit(variable_name + " is blank. Please fill this in to run the bot.")
+            sys.exit(variable_name + " is blank. Please fill this in to run the bot.")
     return env_value
 
 
@@ -63,8 +63,10 @@ def handle_args():
         args.bot_token = check_env_variable(GTS_BOT_TOKEN)
     if args.maps_token == None:
         args.maps_token = check_env_variable(GTS_MAPS_TOKEN)
+    if args.support_server == "":
+        args.support_server = os.getenv(GTS_SUPPORT_SERVER)
     if args.support_server == None:
-        args.support_server = check_env_variable(GTS_SUPPORT_SERVER)
+        args.support_server = check_env_variable(GTS_SUPPORT_SERVER, "")
     if args.db_path == None:
         args.db_path = check_env_variable(GTS_DB_PATH)
     args.db_connection = mysql.connect(user=check_env_variable(GTS_DB_USER), passwd=check_env_variable(GTS_DB_PASS), host=args.db_path, db=check_env_variable(GTS_DB_DATABASE), buffered=True)
